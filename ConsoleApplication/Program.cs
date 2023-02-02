7dc2a5b6a8612e11f7c79ed5bc3bc45efb641d0f
ravi <ravi30445@gmail.com>
2023-02-02 10:49:36 +0530

    added notes in readme

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace HelloWorld;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        int num=15;
+        bool isnumber10=num==10?true:false;
+        Console.WriteLine(isnumber10);
+    }
+}
